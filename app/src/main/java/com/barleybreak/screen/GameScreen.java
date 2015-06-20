ca07f3f9e331aaf5383d7a0bb284258b9c97c836
Ivan Melnikov <ivan9105@bk.ru>
2015-06-20 13:47:17 +0400

    digits on game screen

--- app/src/main/java/com/barleybreak/screen/GameScreen.java
+++ app/src/main/java/com/barleybreak/screen/GameScreen.java
@@ -40,22 +40,22 @@ public class GameScreen extends Screen {
         List<TouchEvent> touchEvents = game.getInput().getTouchEvents();
         game.getInput().getKeyEvents();
 
-        if(state == GameState.Ready)
+        if (state == GameState.Ready)
             updateReady(touchEvents);
-        if(state == GameState.Running)
+        if (state == GameState.Running)
             updateRunning(touchEvents);
-        if(state == GameState.Paused)
+        if (state == GameState.Paused)
             updatePaused(touchEvents);
-        if(state == GameState.GameOver)
+        if (state == GameState.GameOver)
             updateGameOver(touchEvents);
     }
 
     private void updateReady(List<TouchEvent> touchEvents) {
-        if(touchEvents.size() > 0) {
+        if (touchEvents.size() > 0) {
             int len = touchEvents.size();
-            for(int i = 0; i < len; i++) {
+            for (int i = 0; i < len; i++) {
                 TouchEvent event = touchEvents.get(i);
-                if(event.type == TouchEvent.TOUCH_UP) {
+                if (event.type == TouchEvent.TOUCH_UP) {
                     state = GameState.Running;
                 }
             }
@@ -64,28 +64,28 @@ public class GameScreen extends Screen {
 
     private void updateRunning(List<TouchEvent> touchEvents) {
         int len = touchEvents.size();
-        for(int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++) {
             TouchEvent event = touchEvents.get(i);
-            if(event.type == TouchEvent.TOUCH_UP) {
+            if (event.type == TouchEvent.TOUCH_UP) {
                 world.getClickedSquare(event.x, event.y);
 
                 int[] numbs = {1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 0};
-                if(checkGameOver(numbs)) {
+                if (checkGameOver(numbs)) {
                     world.setGameOver(true);
                     state = GameState.GameOver;
                 }
 
-                if(event.x < 64 && event.y < 64) {
-                    if(Settings.soundEnabled)
+                if (event.x < 64 && event.y < 64) {
+                    if (Settings.soundEnabled)
                         Assets.click.play(1);
                     state = GameState.Paused;
                     return;
                 }
             }
 
-            if(event.type == TouchEvent.TOUCH_UP) {
-                if(event.x < 64 && event.y < 64) {
-                    if(Settings.soundEnabled)
+            if (event.type == TouchEvent.TOUCH_UP) {
+                if (event.x < 64 && event.y < 64) {
+                    if (Settings.soundEnabled)
                         Assets.click.play(1);
                     state = GameState.Paused;
                     return;
@@ -96,19 +96,19 @@ public class GameScreen extends Screen {
 
     private void updatePaused(List<TouchEvent> touchEvents) {
         int len = touchEvents.size();
-        for(int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++) {
             TouchEvent event = touchEvents.get(i);
-            if(event.type == Input.TouchEvent.TOUCH_UP) {
-                if(event.x > 60 && event.x <= 240) {
-                    if(event.y > 175 && event.y <= 225) {
-                        if(Settings.soundEnabled)
+            if (event.type == Input.TouchEvent.TOUCH_UP) {
+                if (event.x > 60 && event.x <= 240) {
+                    if (event.y > 175 && event.y <= 225) {
+                        if (Settings.soundEnabled)
                             Assets.click.play(1);
                         state = GameState.Running;
                         return;
                     }
 
-                    if(event.y > 235 && event.y < 285) {
-                        if(Settings.soundEnabled)
+                    if (event.y > 235 && event.y < 285) {
+                        if (Settings.soundEnabled)
                             Assets.click.play(1);
                         game.setScreen(new MainMenuScreen(game));
                         return;
@@ -120,12 +120,12 @@ public class GameScreen extends Screen {
 
     private void updateGameOver(List<TouchEvent> touchEvents) {
         int len = touchEvents.size();
-        for(int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++) {
             Input.TouchEvent event = touchEvents.get(i);
-            if(event.type == Input.TouchEvent.TOUCH_UP) {
-                if(event.x >= 128 && event.x <= 192 &&
+            if (event.type == Input.TouchEvent.TOUCH_UP) {
+                if (event.x >= 128 && event.x <= 192 &&
                         event.y >= 200 && event.y <= 264) {
-                    if(Settings.soundEnabled)
+                    if (Settings.soundEnabled)
                         Assets.click.play(1);
                     game.setScreen(new MainMenuScreen(game));
                     return;
@@ -137,8 +137,8 @@ public class GameScreen extends Screen {
     public boolean checkGameOver(int[] numbs) {
         List<Integer> listN = world.getNums();
         int count = 0;
-        for(int i = 0; i < numbs.length; i++) {
-            if(listN.get(i) == numbs[i]) {
+        for (int i = 0; i < numbs.length; i++) {
+            if (listN.get(i) == numbs[i]) {
                 count++;
             }
         }
@@ -151,13 +151,13 @@ public class GameScreen extends Screen {
         Graphics g = game.getGraphics();
         drawWorld();
 
-        if(state == GameState.Ready)
+        if (state == GameState.Ready)
             drawReadyUI();
-        if(state == GameState.Running)
+        if (state == GameState.Running)
             drawRunningUI();
-        if(state == GameState.Paused)
+        if (state == GameState.Paused)
             drawPausedUI();
-        if(state == GameState.GameOver)
+        if (state == GameState.GameOver)
             drawGameOverUI();
     }
 
@@ -175,8 +175,8 @@ public class GameScreen extends Screen {
     private void drawRunningUI() {
         Graphics g = game.getGraphics();
         List<Square> squares = world.getSquares();
-        for(Square square:squares) {
-            if(square.getValue() != 0) {
+        for (Square square : squares) {
+            if (square.getValue() != 0) {
                 g.drawPixmap(Assets.square, square.getX(), square.getY());
                 drawText(g, String.valueOf(square.getValue()), square.getX() + 22, square.getY() + 17);
             }
@@ -219,7 +219,82 @@ public class GameScreen extends Screen {
                 srcWidth = 20;
             }
 
-            g.drawPixmap(Assets.numbers, x, y, srcX, 0, srcWidth, 32);
+            if (i == 1) {
+                x = x - 10;
+            }
+
+            if (line.equals("1")) {
+                x = x + 5;
+            }
+
+            if (character == '1') {
+                g.drawPixmap(Assets.numbers, x, y, 0, 0, 10, 54);
+            }
+
+            if (character == '2') {
+                //25 x2 - x1
+                //10 позиция x1
+                g.drawPixmap(Assets.numbers, x, y, 10, 0, 25, 54);
+            }
+
+            if (character == '2') {
+                //25 x2 - x1
+                //10 позиция x1
+                g.drawPixmap(Assets.numbers, x, y, 10, 0, 25, 54);
+            }
+
+            if (character == '3') {
+                g.drawPixmap(Assets.numbers, x, y, 36, 0, 24, 54);
+            }
+
+            if (character == '4') {
+                g.drawPixmap(Assets.numbers, x, y, 61, 0, 27, 54);
+            }
+
+            if (character == '5') {
+                g.drawPixmap(Assets.numbers, x, y, 89, 0, 22, 54);
+            }
+
+            if (character == '6') {
+                g.drawPixmap(Assets.numbers, x, y, 112, 0, 22, 54);
+            }
+
+            if (character == '7') {
+                g.drawPixmap(Assets.numbers, x, y, 138, 0, 21, 54);
+            }
+
+            if (character == '8') {
+                g.drawPixmap(Assets.numbers, x, y, 160, 0, 19, 54);
+            }
+
+            if (character == '7') {
+                g.drawPixmap(Assets.numbers, x, y, 138, 0, 21, 54);
+            }
+
+            if (character == '8') {
+                g.drawPixmap(Assets.numbers, x, y, 160, 0, 19, 54);
+            }
+
+            if (character == '9') {
+                g.drawPixmap(Assets.numbers, x, y, 179, 0, 23, 54);
+            }
+
+            if (character == '0') {
+                g.drawPixmap(Assets.numbers, x, y, 203, 0, 24, 54);
+            }
+
+            if (character == '.') {
+                g.drawPixmap(Assets.numbers, x, y, 228, 0, 10, 54);
+            }
+
+            if (character == ':') {
+                g.drawPixmap(Assets.numbers, x, y, 239, 0, 10, 54);
+            }
+
+            if (character == '-') {
+                g.drawPixmap(Assets.numbers, x, y, 250, 0, 14, 54);
+            }
+
             x += srcWidth;
         }
     }
